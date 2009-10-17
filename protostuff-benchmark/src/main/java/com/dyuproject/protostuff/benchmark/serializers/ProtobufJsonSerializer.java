919551b8ac89b93c83a226299f9caa9e3288e628
dyuproject <dyuproject@3b38d51a-9144-11de-8ad7-8b2c2f1e2016>
2009-10-17 01:27:39 +0000

    subclass instead
    git-svn-id: http://protostuff.googlecode.com/svn/trunk@100 3b38d51a-9144-11de-8ad7-8b2c2f1e2016

--- protostuff-benchmark/src/main/java/com/dyuproject/protostuff/benchmark/serializers/ProtobufJsonSerializer.java
+++ protostuff-benchmark/src/main/java/com/dyuproject/protostuff/benchmark/serializers/ProtobufJsonSerializer.java
@@ -4,59 +4,17 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 
 import com.dyuproject.protostuff.benchmark.V22SpeedMedia;
-import com.dyuproject.protostuff.benchmark.V22SpeedMedia.Image;
-import com.dyuproject.protostuff.benchmark.V22SpeedMedia.Media;
-import com.dyuproject.protostuff.benchmark.V22SpeedMedia.MediaContent;
-import com.dyuproject.protostuff.benchmark.V22SpeedMedia.Image.Size;
-import com.dyuproject.protostuff.benchmark.V22SpeedMedia.Media.Player;
 import com.google.protobuf.JsonFormat;
 
 /**
  * Copied from http://thrift-protobuf-compare.googlecode.com/svn/trunk/tpc/src/serializers/
+ * Tweaked a bit to subclass AbstractSpeedMediaSerializer
  *
  */
-public class ProtobufJsonSerializer implements ObjectSerializer<V22SpeedMedia.MediaContent>
+public class ProtobufJsonSerializer extends AbstractSpeedMediaSerializer
 {
-    private final Charset _charset = Charset.forName("UTF-8");
     
-    public MediaContent create()
-    {
-        MediaContent contentProto = MediaContent
-        .newBuilder().setMedia(
-                Media.newBuilder()
-                        .clearCopyright()
-                        .setFormat("video/mpg4")
-                        .setPlayer(Player.JAVA)
-                        .setTitle("Javaone Keynote")
-                        .setUri("http://javaone.com/keynote.mpg")
-                        .setDuration(1234567)
-                        .setSize(123)
-                        .setHeight(0)
-                        .setWidth(0)
-                        .setBitrate(123)
-                        .addPerson("Bill Gates")
-                        .addPerson("Steve Jobs")
-                        .build()
-                ).addImage(
-                        Image.newBuilder()
-                        .setHeight(0)
-                        .setTitle("Javaone Keynote")
-                        .setUri("http://javaone.com/keynote_large.jpg")
-                        .setWidth(0)
-                        .setSize(Size.LARGE)
-                        .build()
-                ).addImage(
-                        Image.newBuilder()
-                        .setHeight(0)
-                        .setTitle("Javaone Keynote")
-                        .setUri("http://javaone.com/keynote_thumbnail.jpg")
-                        .setWidth(0)
-                        .setSize(Size.SMALL)
-                        .build()
-                )
-                .build();
-        return contentProto;
-    }
+    private final Charset _charset = Charset.forName("UTF-8");
 
     public V22SpeedMedia.MediaContent deserialize (byte[] array) throws Exception
     {
