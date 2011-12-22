d4a12972f0f6da376cc075e8580f99ba6ec58cae
dyuproject@gmail.com <dyuproject@gmail.com@3b38d51a-9144-11de-8ad7-8b2c2f1e2016>
2011-12-22 12:08:10 +0000

    Use ISE for exceptions on codegen.
    git-svn-id: http://protostuff.googlecode.com/svn/trunk@1438 3b38d51a-9144-11de-8ad7-8b2c2f1e2016

--- protostuff-parser/src/main/java/com/dyuproject/protostuff/parser/ErrorMap.java
+++ protostuff-parser/src/main/java/com/dyuproject/protostuff/parser/ErrorMap.java
@@ -54,7 +54,7 @@ public final class ErrorMap implements Map<String,Object>
     public boolean containsKey(Object arg0)
     {
         // error mechanism called by stringtemplate
-        throw new RuntimeException(String.valueOf(arg0));
+        throw new IllegalStateException(String.valueOf(arg0));
     }
 
     public Object get(Object arg0)
