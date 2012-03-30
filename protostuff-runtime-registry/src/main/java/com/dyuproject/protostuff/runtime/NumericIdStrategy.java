0686c1aeac22ee49aaef5a0d2ea70342ef419575
dyuproject@gmail.com <dyuproject@gmail.com@3b38d51a-9144-11de-8ad7-8b2c2f1e2016>
2012-03-30 04:22:07 +0000

    make it static
    git-svn-id: http://protostuff.googlecode.com/svn/trunk@1463 3b38d51a-9144-11de-8ad7-8b2c2f1e2016

--- protostuff-runtime-registry/src/main/java/com/dyuproject/protostuff/runtime/NumericIdStrategy.java
+++ protostuff-runtime-registry/src/main/java/com/dyuproject/protostuff/runtime/NumericIdStrategy.java
@@ -118,7 +118,7 @@ public abstract class NumericIdStrategy extends IdStrategy
             resolveClass(input.readUInt32());
     }
     
-    private int getIdPrimitive(Class<?> clazz)
+    private static int getIdPrimitive(Class<?> clazz)
     {
         return RuntimeFieldFactory.getInline(clazz).id - 1;
     }
