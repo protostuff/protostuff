0864b5c33717bf5c00cbd57ac010ee98a62d62e3
Konstantin Shchepanovskyi <schepanovsky@gmail.com>
2016-05-08 23:31:14 +0300

    https://github.com/protostuff/protostuff-compiler/issues/28: Add ListAdapter for new java generated code
     Used same approach as in protobuf: enums are stored as integer value (list of integers).
     ListAdapter is needed in order to provide conversion of integer values to enum values
     on the fly when accessing list, instead of using additional memory.

---
+++ protostuff-api/src/test/java/io/protostuff/ListAdapterTest.java
@@ -0,0 +1,36 @@
+package io.protostuff;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Kostiantyn Shchepanovskyi
+ */
+public class ListAdapterTest
+{
+
+    @Test
+    public void checkConvertedValues() throws Exception
+    {
+        List<Integer> a = new ArrayList<>();
+        ListAdapter<Integer, String> adapter = new ListAdapter<>(a, new ListAdapter.Converter<Integer, String>()
+        {
+            @Override
+            public String convert(Integer from)
+            {
+                return from.toString();
+            }
+        });
+        a.add(5);
+        a.add(10);
+        Assert.assertEquals(2, adapter.size());
+        Assert.assertEquals("5", adapter.get(0));
+        Assert.assertEquals("10", adapter.get(1));
+
+    }
+}
