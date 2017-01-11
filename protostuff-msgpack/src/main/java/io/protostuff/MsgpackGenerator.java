9d6924197e1af852e0cf44a3b2843c17ddcb88a3
Alex Shvid <a@shvid.com>
2017-01-11 10:13:07 -0800

    move MsgpackArrayValueImpl into MsgpackGenerator, because only this class is using it

--- protostuff-msgpack/src/main/java/io/protostuff/MsgpackGenerator.java
+++ protostuff-msgpack/src/main/java/io/protostuff/MsgpackGenerator.java
@@ -16,13 +16,30 @@ package io.protostuff;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.msgpack.core.MessagePacker;
+import org.msgpack.core.MessageTypeCastException;
+import org.msgpack.value.ArrayValue;
+import org.msgpack.value.ImmutableArrayValue;
+import org.msgpack.value.ImmutableBinaryValue;
+import org.msgpack.value.ImmutableBooleanValue;
+import org.msgpack.value.ImmutableExtensionValue;
+import org.msgpack.value.ImmutableFloatValue;
+import org.msgpack.value.ImmutableIntegerValue;
 import org.msgpack.value.ImmutableMapValue;
+import org.msgpack.value.ImmutableNilValue;
+import org.msgpack.value.ImmutableNumberValue;
+import org.msgpack.value.ImmutableRawValue;
+import org.msgpack.value.ImmutableStringValue;
+import org.msgpack.value.ImmutableValue;
 import org.msgpack.value.Value;
+import org.msgpack.value.ValueType;
+import org.msgpack.value.impl.ImmutableArrayValueImpl;
 import org.msgpack.value.impl.ImmutableLongValueImpl;
 import org.msgpack.value.impl.ImmutableMapValueImpl;
+import org.msgpack.value.impl.ImmutableNilValueImpl;
 import org.msgpack.value.impl.ImmutableStringValueImpl;
 
 /**
@@ -67,9 +84,9 @@ public final class MsgpackGenerator
         return this;
     }
 
-    private MsgpackArrayValueImpl getLastArray()
+    private ArrayValueImpl getLastArray()
     {
-        return (MsgpackArrayValueImpl) kvs.get(kvs.size() - 1);
+        return (ArrayValueImpl) kvs.get(kvs.size() - 1);
     }
 
     public void pushValue(Schema<?> schema, int fieldNumber, Value value, boolean repeated)
@@ -96,7 +113,7 @@ public final class MsgpackGenerator
 
         if (repeated)
         {
-            MsgpackArrayValueImpl array = new MsgpackArrayValueImpl();
+            ArrayValueImpl array = new ArrayValueImpl();
             array.add(value);
 
             kvs.add(array);
@@ -119,6 +136,283 @@ public final class MsgpackGenerator
     public ImmutableMapValue toValue()
     {
         return new ImmutableMapValueImpl(kvs.toArray(new Value[kvs.size()]));
+    }
+
+    /**
+     * Abstract value adaptor class
+     * 
+     * @author Alex Shvid
+     *
+     */
+
+    public static abstract class AbstractValue implements Value
+    {
+
+        @Override
+        public boolean isNilValue()
+        {
+            return getValueType().isNilType();
+        }
+
+        @Override
+        public boolean isBooleanValue()
+        {
+            return getValueType().isBooleanType();
+        }
+
+        @Override
+        public boolean isNumberValue()
+        {
+            return getValueType().isNumberType();
+        }
+
+        @Override
+        public boolean isIntegerValue()
+        {
+            return getValueType().isIntegerType();
+        }
+
+        @Override
+        public boolean isFloatValue()
+        {
+            return getValueType().isFloatType();
+        }
+
+        @Override
+        public boolean isRawValue()
+        {
+            return getValueType().isRawType();
+        }
+
+        @Override
+        public boolean isBinaryValue()
+        {
+            return getValueType().isBinaryType();
+        }
+
+        @Override
+        public boolean isStringValue()
+        {
+            return getValueType().isStringType();
+        }
+
+        @Override
+        public boolean isArrayValue()
+        {
+            return getValueType().isArrayType();
+        }
+
+        @Override
+        public boolean isMapValue()
+        {
+            return getValueType().isMapType();
+        }
+
+        @Override
+        public boolean isExtensionValue()
+        {
+            return getValueType().isExtensionType();
+        }
+
+        @Override
+        public ImmutableNilValue asNilValue()
+        {
+            throw new MessageTypeCastException();
+        }
+
+        @Override
+        public ImmutableBooleanValue asBooleanValue()
+        {
+            throw new MessageTypeCastException();
+        }
+
+        @Override
+        public ImmutableNumberValue asNumberValue()
+        {
+            throw new MessageTypeCastException();
+        }
+
+        @Override
+        public ImmutableIntegerValue asIntegerValue()
+        {
+            throw new MessageTypeCastException();
+        }
+
+        @Override
+        public ImmutableFloatValue asFloatValue()
+        {
+            throw new MessageTypeCastException();
+        }
+
+        @Override
+        public ImmutableRawValue asRawValue()
+        {
+            throw new MessageTypeCastException();
+        }
+
+        @Override
+        public ImmutableBinaryValue asBinaryValue()
+        {
+            throw new MessageTypeCastException();
+        }
+
+        @Override
+        public ImmutableStringValue asStringValue()
+        {
+            throw new MessageTypeCastException();
+        }
+
+        @Override
+        public ImmutableArrayValue asArrayValue()
+        {
+            throw new MessageTypeCastException();
+        }
+
+        @Override
+        public ImmutableMapValue asMapValue()
+        {
+            throw new MessageTypeCastException();
+        }
+
+        @Override
+        public ImmutableExtensionValue asExtensionValue()
+        {
+            throw new MessageTypeCastException();
+        }
+
+    }
+
+    /**
+     * Implementation of the msgpack mutable array
+     * 
+     * @author Alex Shvid
+     *
+     */
+
+    public static final class ArrayValueImpl extends AbstractValue implements ArrayValue
+    {
+
+        private final List<Value> list = new ArrayList<Value>();
+
+        public void reset()
+        {
+            list.clear();
+        }
+
+        public void add(Value value)
+        {
+            list.add(value);
+        }
+
+        @Override
+        public ImmutableValue immutableValue()
+        {
+            return new ImmutableArrayValueImpl(list.toArray(new Value[list.size()]));
+        }
+
+        @Override
+        public void writeTo(MessagePacker packer) throws IOException
+        {
+            int size = list.size();
+            packer.packArrayHeader(size);
+            for (int i = 0; i != size; i++)
+            {
+                list.get(i).writeTo(packer);
+            }
+        }
+
+        @Override
+        public String toJson()
+        {
+            int size = list.size();
+            if (size == 0)
+            {
+                return "[]";
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.append("[");
+            sb.append(list.get(0).toJson());
+            for (int i = 1; i != size; i++)
+            {
+                sb.append(",");
+                sb.append(list.get(0).toJson());
+            }
+            sb.append("]");
+            return sb.toString();
+        }
+
+        @Override
+        public String toString()
+        {
+            int size = list.size();
+            if (size == 0)
+            {
+                return "[]";
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.append("[");
+            appendString(sb, list.get(0));
+            for (int i = 1; i != size; i++)
+            {
+                sb.append(",");
+                appendString(sb, list.get(i));
+            }
+            sb.append("]");
+            return sb.toString();
+        }
+
+        private void appendString(StringBuilder sb, Value value)
+        {
+            if (value.isRawValue())
+            {
+                sb.append(value.toJson());
+            }
+            else
+            {
+                sb.append(value.toString());
+            }
+        }
+
+        @Override
+        public int size()
+        {
+            return list.size();
+        }
+
+        @Override
+        public Value get(int index)
+        {
+            return list.get(index);
+        }
+
+        @Override
+        public Value getOrNilValue(int index)
+        {
+            if (index < list.size() && index >= 0)
+            {
+                return list.get(index);
+            }
+            return ImmutableNilValueImpl.get();
+        }
+
+        @Override
+        public Iterator<Value> iterator()
+        {
+            return list.iterator();
+        }
+
+        @Override
+        public List<Value> list()
+        {
+            return list;
+        }
+
+        @Override
+        public ValueType getValueType()
+        {
+            return ValueType.ARRAY;
+        }
+
     }
 
 }
